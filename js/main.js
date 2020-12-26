f224d85ee2022c90265e4844a2b16b1f7333d8d5
Adi Gonnen <adigonnen@gmail.com>
2020-12-26 22:21:16 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 'use strict'
-function init() {
-    //
-}
 
-function showGallery() {
-   console.log('start')
+function init() {
+    const today = new Date();
+    const dd = today.getDate();
+    const mm = today.getMonth() + 1;
+    let yy =`${today.getFullYear()}`.slice(0,2);
+    const fullDate = dd + '.' + mm + '.' + yy;
+    const list = document.getElementsByClassName('date')
+    list[0].innerHTML = fullDate;
+    list[1].innerHTML = fullDate;
 }
 
