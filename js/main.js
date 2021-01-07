72b0c00b6b884a9675912faa47366e242f856cdd
Adi Gonnen <adigonnen@gmail.com>
2021-01-07 20:12:44 +0200

    fix date-year

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function init() {
     const today = new Date();
     const dd = today.getDate();
     const mm = today.getMonth() + 1;
-    let yy =`${today.getFullYear()}`.slice(0,2);
+    let yy =`${today.getFullYear()}`.slice(-2);
     const fullDate = dd + '.' + mm + '.' + yy;
     const list = document.getElementsByClassName('date')
     list[0].innerHTML = fullDate;
